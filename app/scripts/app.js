e575ca9f7670e1f201165912358cbdc89b135ea0
Todd Carlson <tcarlson@carlsonsw.com>
2017-11-25 19:53:49 -0500

    checkpoint 5: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,8 +11,14 @@ angular.module('blocJams', ['ui.router']);
  	    $stateProvider
     	     .state('landing', {
        	    	 url: '/',
+                 controller: 'LandingCtrl as landing',       	    	 
        	     	 templateUrl: '/templates/landing.html'
        		 });
+ 			 .state('collection', {
+     			url: '/collection',
+    			controller: 'CollectionCtrl as collection',
+     			templateUrl: '/templates/collection.html'
+ 			 });       		 
         	 .state('album', {
             	 url: '/album',
             	 templateUrl: '/templates/album.html'
