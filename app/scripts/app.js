2f5a8630d60e2d388675610b67c5218d457c78b1
Todd Carlson <tcarlson@carlsonsw.com>
2017-11-29 19:09:56 -0500

    fixed passed issue

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,18 +12,17 @@ angular.module('blocJams', ['ui.router']);
     	     .state('landing', {
        	    	 url: '/',
        	     	 templateUrl: '/templates/landing.html'
-       		 });
+       		 })
         	 .state('album', {
             	 url: '/album',
             	 templateUrl: '/templates/album.html'
-         	 });
+         	 })
         	 .state('collection', {
             	 url: '/collection',
             	 templateUrl: '/templates/collection.html'
          	 });
      }
  
- angular.module('blocJams', ['ui.router']);
      angular
          .module('blocJams', ['ui.router'])
          .config(config);
