9058c9ea442abade225f04857d11dc7893711f3e
Todd Carlson <tcarlson@carlsonsw.com>
2018-01-11 20:30:19 -0500

    directives part-2 1.11.18, attempt 2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -30,7 +30,12 @@
                      SongPlayer.currentTime = currentBuzzObject.getTime();
                  });
              });
-
+/*
+            currentBuzzObject.bind("volumechange", function () {
+                  $rootScope.$apply(function() {
+                     SongPlayer.volume = currentBuzzObject.getVolume();
+            });
+*/
              SongPlayer.currentSong = song;
          };
          /**
